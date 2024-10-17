71a7230e103aee8c642edfa7fd13dbd385882b51
Kapil Madankar <108929223+kpl-2704@users.noreply.github.com>
2024-10-17 16:09:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p></p>
+        <a
+          className="App-link"
+          href="https://media.istockphoto.com/id/1452604857/photo/businessman-touching-the-brain-working-of-artificial-intelligence-automation-predictive.webp?b=1&s=612x612&w=0&k=20&c=5LyslY4gssQ99CVUrUB2K75Mx2TFgnkxboAqB38OPUQ="
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React Native & Native Native Development & Native Development
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
